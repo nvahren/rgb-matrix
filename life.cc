4cc3201df95fd6cd9f5cde25526b023f023d0755
Nathan Vahrenberg <nvahren@gmail.com>
2021-03-12 18:17:22 -0600

    basic demo

--- life.cc
+++ life.cc
@@ -15,7 +15,7 @@ static void InterruptHandler(int signo) {
 }
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
+    if (argc != 3) {
         cout << "Usage: ./rgb-matrix [rows] [columns]" << endl;
         exit(EXIT_FAILURE);
     }
